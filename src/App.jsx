f94ccac32143046007c06772a01196d769a63dc3
Pasin Piampowdee <pangton28.p@gmail.com>
2024-03-19 17:43:17 +0700

    add add default files for creating a website

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className=" bg-red-500 flex justify-between">
+          <div className='m-4'>
+            <p>My Profile</p>
+          </div>
+          <div className='flex flex-row gap-6 m-4'>
+            <p>About Me</p>
+            <p>Project</p>
+            <p>Contact</p>
+            <p>Resume</p>
+          </div>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
